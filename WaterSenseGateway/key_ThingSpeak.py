1948142677b90545e0575ef368fce9f9b203006d
Congduc Pham <cpham@scinfr93.univ-pau.fr>
2017-04-25 10:58:23 +0200

    First commit

---
+++ WaterSenseGateway/key_ThingSpeak.py
@@ -0,0 +1,2 @@
+# WaterSense LoRa demo channel
+_def_thingspeak_channel_key='H95DRC6RZ6J2CUQF'
---
+++ WaterSenseGateway/key_WAZIUP.py
@@ -0,0 +1,27 @@
+####################################################
+#server: CAUTION must exist
+waziup_server="http://broker.waziup.io/v2"
+
+#project name
+project_name="watersense"
+
+#your organization: CHANGE HERE
+organization_name="FARM1"
+
+#service tree: CHANGE HERE at your convenience
+#should start with /
+service_tree='/TESTS'
+
+#sensor name: CHANGE HERE but maybe better to leave it as Sensor
+#the final name will contain the sensor address
+sensor_name="WSSensor"
+
+#service path: DO NOT CHANGE HERE
+service_path='/'+organization_name+service_tree
+
+#SUMMARY
+#the entity name will then be sensor_name+scr_addr, e.g. "WSSensor1"
+#the Fiware-ServicePath will be service_path which is based on both organization_name and service_tree, e.g. "/FARM1/TESTS"
+#the Fiware-Service will be project_name, e.g. "watersense"
+
+####################################################
